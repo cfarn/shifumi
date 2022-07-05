dc631be5261833dee8f310081ebfffb63a438db3
cfarn <58476975+cfarn@users.noreply.github.com>
2022-07-05 15:37:05 +0200

    ajout js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let player; ia
+let player, ia, iaTxt
 
 const iaRandom = document.getElementById('ia')
 const signImg = document.getElementById('playerImg')
@@ -7,8 +7,17 @@ const resultDisplay = document.getElementById('gameResult')
 // Choix du joueur
 const playerChoice = (choice) => {
     player = choice
+    let img 
+    if(player==="stone"){
+        img = 1
+    }else if(player === "paper"){
+        img = 2
+    }else if(player === "scissors") {
+        img = 3
+    }
     // image du choix
-    signImg.innerHTML = `<img src="img/${playerImg}.jpg" alt="" id="signImg">`
+    signImg.innerHTML = `<img src="img/${img}.jpg" alt="" id="signImg">`
+    console.log(playerImg)
     // console.log("I clicked on my button")
     iaChoice()
 
@@ -22,6 +31,13 @@ const iaChoice = (choice) => {
     choice = Math.floor(Math.random() * max) + min
 
     ia = choice
+    if(ia === 1) {
+        iaTxt = "stone"
+    }else if(ia === 2) {
+        iaTxt = "paper"
+    }else if(ia === 3) {
+        iaTxt = "scissors"
+    }
 
     // image du choix
     iaRandom.innerHTML = `<img src="img/${ia}.jpg" alt="" id="iaRandom">`
@@ -37,19 +53,18 @@ const iaChoice = (choice) => {
 
 // Déterminer le vainqueur
 const winner = () => {
-    
-    if((player === "stone" && ia === "scissors") || (player === "scissors" && ia === "paper") || (player === "paper" && ia === "stone")) {
+    if((player === "stone" && iaTxt === "scissors") || (player === "scissors" && iaTxt === "paper") || (player === "paper" && iaTxt === "stone")) {
         console.log("Vous avez gagné !") 
-        // result = "Vous avez gagné !"
-    }else if((player === "scissors" && ia === "stone") || (player === "paper" && ia === "scissors") || (player === "stone" && ia === "paper")) {
+        result = "Vous avez gagné !"
+    }else if((player === "scissors" && iaTxt === "stone") || (player === "paper" && iaTxt === "scissors") || (player === "stone" && iaTxt === "paper")) {
         console.log("Vous avez perdu")
-        // result = "Vous avez perdu"
-    }else if(player === ia) {
+        result = "Vous avez perdu"
+    }else if(player === iaTxt) {
         console.log("Match nul")
-        // result = "Match nul"
+        result = "Match nul"
     }
     // afficher le résultat
-    // gameResult.innerHTML = result
+    gameResult.innerHTML = result
 }
 
 
